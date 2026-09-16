23088da91923b715655cfbc436d9ed61bb728148
feliarroyo <farroyo@alumnos.exa.unicen.edu.ar>
2026-09-16 09:37:00 -0300

    fix(backend): fasecontroller imports

--- backend/qualititrack/src/main/java/com/backend/qualititrack/Controller/FaseController.java
+++ backend/qualititrack/src/main/java/com/backend/qualititrack/Controller/FaseController.java
@@ -1,21 +1,22 @@
 package com.backend.qualititrack.Controller;
 
-import com.backend.qualititrack.modelos.Fase;
-import com.backend.qualititrack.modelos.FaseCatalogo;
-import com.backend.qualititrack.Service.FaseService;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.backend.qualititrack.Service.FaseService;
 import com.backend.qualititrack.modelos.FaseCatalogo;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/fases")
