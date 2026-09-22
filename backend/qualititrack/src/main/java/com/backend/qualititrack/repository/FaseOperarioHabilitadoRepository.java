f847cdc81dfa1121008dc4646d329c51ec39d42c
feliarroyo <farroyo@alumnos.exa.unicen.edu.ar>
2026-09-22 15:15:10 -0300

    feat(backend): agregado método para buscar operarios de una fase

--- backend/qualititrack/src/main/java/com/backend/qualititrack/repository/FaseOperarioHabilitadoRepository.java
+++ backend/qualititrack/src/main/java/com/backend/qualititrack/repository/FaseOperarioHabilitadoRepository.java
@@ -1,5 +1,6 @@
 package com.backend.qualititrack.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +10,9 @@ import com.backend.qualititrack.modelos.FaseOperarioHabilitado;
 
 @Repository
 public interface FaseOperarioHabilitadoRepository extends JpaRepository<FaseOperarioHabilitado, Long> {
+    // Utilizado para buscar si un operario ya se encuentra habilitado en una fase específica
     Optional<FaseOperarioHabilitado> findByFaseCatalogoIdAndOperarioId(Long faseId, Long operarioId);
+
+    // Utilizado para buscar operarios habilitados para una fase específica
+    List<FaseOperarioHabilitado> findByFaseCatalogoIdAndHabilitadoTrue(Long faseCatalogoId);
 }
