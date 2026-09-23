2d5b2cdbd1c552c8e8bb7b0367e9b3f48874dfde
feliarroyo <farroyo@alumnos.exa.unicen.edu.ar>
2026-09-23 15:13:07 -0300

    fix(backend): chequear habilitación por fase

--- backend/qualititrack/src/main/java/com/backend/qualititrack/repository/FaseOperarioHabilitadoRepository.java
+++ backend/qualititrack/src/main/java/com/backend/qualititrack/repository/FaseOperarioHabilitadoRepository.java
@@ -10,8 +10,12 @@ import com.backend.qualititrack.modelos.FaseOperarioHabilitado;
 
 @Repository
 public interface FaseOperarioHabilitadoRepository extends JpaRepository<FaseOperarioHabilitado, Long> {
-    // Utilizado para buscar si un operario ya se encuentra habilitado en una fase específica
+    
+    // Para ver si ya hay una entrada generada.
     Optional<FaseOperarioHabilitado> findByFaseCatalogoIdAndOperarioId(Long faseId, Long operarioId);
+
+    // Utilizado para buscar si un operario ya se encuentra habilitado en una fase específica
+    Optional<FaseOperarioHabilitado> findByFaseCatalogoIdAndOperarioIdAndHabilitadoTrue(Long faseId, Long operarioId);
 
     // Utilizado para buscar operarios habilitados para una fase específica
     List<FaseOperarioHabilitado> findByFaseCatalogoIdAndHabilitadoTrue(Long faseCatalogoId);
