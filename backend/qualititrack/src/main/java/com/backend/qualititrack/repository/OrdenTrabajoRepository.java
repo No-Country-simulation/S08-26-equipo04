aab215580d54bfe70faf3b25310b611485541a88
polyglisdev <ramimarinette@gmail.com>
2026-09-23 18:03:13 -0400

    feat: agrega endpoint de OTs pendientes de auditoria

--- backend/qualititrack/src/main/java/com/backend/qualititrack/repository/OrdenTrabajoRepository.java
+++ backend/qualititrack/src/main/java/com/backend/qualititrack/repository/OrdenTrabajoRepository.java
@@ -1,12 +1,17 @@
 package com.backend.qualititrack.repository;
 
+import com.backend.qualititrack.Enum.EstadoOT;
 import com.backend.qualititrack.modelos.OrdenTrabajo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface OrdenTrabajoRepository extends JpaRepository<OrdenTrabajo, Long> {
+
     Optional<OrdenTrabajo> findByCotizacionId(Long cotizacionId);
+
+    List<OrdenTrabajo> findByEstado(EstadoOT estado);
 }
