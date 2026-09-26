f7bd0262e000896e3b1bd71acf90e79b6d73c23b
feliarroyo <farroyo@alumnos.exa.unicen.edu.ar>
2026-09-25 21:29:05 -0300

    fix(backend): mejorar busqueda de operarios

--- backend/qualititrack/src/main/java/com/backend/qualititrack/repository/FaseOperarioHabilitadoRepository.java
+++ backend/qualititrack/src/main/java/com/backend/qualititrack/repository/FaseOperarioHabilitadoRepository.java
@@ -4,9 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.backend.qualititrack.modelos.FaseOperarioHabilitado;
+import com.backend.qualititrack.modelos.Usuario;
 
 @Repository
 public interface FaseOperarioHabilitadoRepository extends JpaRepository<FaseOperarioHabilitado, Long> {
@@ -15,8 +18,12 @@ public interface FaseOperarioHabilitadoRepository extends JpaRepository<FaseOper
     Optional<FaseOperarioHabilitado> findByFaseCatalogoIdAndOperarioId(Long faseId, Long operarioId);
 
     // Utilizado para buscar si un operario ya se encuentra habilitado en una fase específica
-    Optional<FaseOperarioHabilitado> findByFaseCatalogoIdAndOperarioIdAndHabilitadoTrue(Long faseId, Long operarioId);
+    boolean existsByFaseCatalogoIdAndOperarioIdAndHabilitadoTrue(Long faseId, Long operarioId);
 
     // Utilizado para buscar operarios habilitados para una fase específica
-    List<FaseOperarioHabilitado> findByFaseCatalogoIdAndHabilitadoTrue(Long faseCatalogoId);
+    @Query("SELECT foh.operario FROM FaseOperarioHabilitado foh " +
+           "WHERE foh.faseCatalogo.id = :fase_id " +
+           "AND foh.habilitado = true " +
+           "AND foh.operario.activo = true")
+    List<Usuario> findOperariosHabilitadosByFaseId(@Param("fase_id") Long faseId);
 }
